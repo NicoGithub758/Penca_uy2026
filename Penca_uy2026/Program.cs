5a160a5fa8002c65289e498eb3f379ee202f3e22
Santi <santipereyra64@gmail.com>
2026-05-18 19:52:11 -0300

    prueba listado

--- Penca_uy2026/Program.cs
+++ Penca_uy2026/Program.cs
@@ -129,7 +129,7 @@ using (var scope = app.Services.CreateScope())
     try
     {
         var db = services.GetRequiredService<MyDbContext>();
-        db.Database.Migrate();
+       // db.Database.Migrate();
     }
     catch (Exception ex)
     {
