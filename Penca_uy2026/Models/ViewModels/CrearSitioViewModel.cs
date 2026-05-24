8666c3d59e4e6599071877fb422f976a6bebf8b7
Santi <santipereyra64@gmail.com>
2026-05-24 18:38:46 -0300

    prueba link de invitacion

--- Penca_uy2026/Models/ViewModels/CrearSitioViewModel.cs
+++ Penca_uy2026/Models/ViewModels/CrearSitioViewModel.cs
@@ -8,7 +8,6 @@ namespace Penca_uy2026.Models.ViewModels
         public string UrlVercel { get; set; }
         public string NombreAdmin { get; set; }
         public string EmailAdmin { get; set; }
-        public string PasswordAdmin { get; set; }
         public TipoRegistro TipoRegistro { get; set; }
     }
 }
